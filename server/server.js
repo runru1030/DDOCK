a61bd9aa46d30c17ee19b3f044b55fcc477465f8
runru1030 <runru1030@gmail.com>
2021-05-16 02:12:45 +0900

    build heroku

--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ var CryptoJS = require("crypto-js");
 var SHA256 = require("crypto-js/sha256");
 var Base64 = require("crypto-js/enc-base64");
 
-app.use(express.static(path.join(__dirname, '/src/build')));
+app.use(express.static(path.join(__dirname, '../src/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/src/build/index.html'));
+  res.sendFile(path.join(__dirname+'../src/build/index.html'));
 });
 app.use('/api', api);
 app.use(cors());
